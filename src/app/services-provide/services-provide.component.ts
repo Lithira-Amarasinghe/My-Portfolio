e94c20fb34c725b493a61f1bde02ecee0b157b1c
Lithira Amarasinghe <amarasinghelithira@gmail.com>
2025-02-10 21:24:32 +0530

    In services section main sevice image extenstion changed from png to jpg

--- src/app/services-provide/services-provide.component.ts
+++ src/app/services-provide/services-provide.component.ts
@@ -11,21 +11,21 @@ export class ServicesProvideComponent {
     {
       title: 'Portfolio sites',
       icon:'icons/online-resume.png',
-      image: "services/service-01.png",
+      image: "services/service-01.jpg",
       description: 'Create a stunning portfolio website to showcase your work and attract more clients or opportunities.'
     }, {
       title: 'Online Ordering System',
-      image: "services/service-06.png",
+      image: "services/service-06.jpg",
       icon:'icons/purchase.png',
       description: 'Streamline sales with a user-friendly online ordering system, making purchasing easy for your customers.'
     }, {
       title: 'Digitize Processes',
-      image: "services/service-03.png",
+      image: "services/service-03.jpg",
       icon:'icons/digital-business.png',
       description: 'Transform manual tasks into efficient digital solutions, saving time and improving business productivity.'
     }, {
       title: 'Mobile Apps',
-      image: "services/service-05.png",
+      image: "services/service-05.jpg",
       icon:'icons/app.png',
       description: 'Professional mobile apps tailored to your needs, ensuring smooth performance on both Android and iOS.'
     }]
